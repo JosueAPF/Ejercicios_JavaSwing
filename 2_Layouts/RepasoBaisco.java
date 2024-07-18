f08e999050a9ccdb28ab1f2405dae0980825347d
josue <josueporras124@gmail.com>
2024-07-18 09:15:36 -0600

    Update Layouts

--- 2_Layouts/RepasoBaisco.java
+++ 2_Layouts/RepasoBaisco.java
@@ -1,3 +1,9 @@
+/*
+   ide: JGrasp :)
+   FlowLayout: para los JPanle's
+   BorderLayout: para los hijos de JFrame
+*/
+
 //lib principal de ventanas y componentes
 import javax.swing.*;
 //uso de layouts
@@ -7,21 +13,33 @@ public class RepasoBaisco{
    public static void main(String[] args){
       //Creacion de la ventana principal
       JFrame frame = new JFrame("Ventana Principal");
-
-
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      
       JPanel Contenedor1 = new JPanel(new FlowLayout());
+      //borde para el JPanel Contenedor1
+      Contenedor1.setBorder(BorderFactory.createLineBorder(Color.BLUE, 6)); // Establecer borde y color
+      //Control del Tamaño del JPanel o Contenedor1
+      Contenedor1.setPreferredSize(new Dimension(150,100));
+      //Colores para los contenedores JPanle
+      Contenedor1.setBackground(Color.ORANGE);
       JLabel Label1 = new JLabel("Esta es una Prueba de posicionamiento");
       JButton boton1 = new JButton("Click me :)");
-      Contenedor1.add(label1);
+      //Agregamos los componenetes a los contenedores
+      Contenedor1.add(Label1);
       Contenedor1.add(boton1);
             
       JPanel Contenedor2 = new JPanel(new FlowLayout());
-      
+      Contenedor2.setBorder(BorderFactory.createLineBorder(Color.BLACK, 6));
+      Contenedor2.setPreferredSize(new Dimension(150,100));
+
       
       frame.add(Contenedor1 , BorderLayout.NORTH);
+      frame.add(Contenedor2 , BorderLayout.CENTER);
+
       
       //Visualizacion de la ventana
-      frame.setSize(400, 300);
+      frame.setSize(600, 300);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setVisible(true);
    }
